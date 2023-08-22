6df2bc847e71de715665b7cf1caad5d22ba5ee0e
hans <hmrtn@pm.me>
2023-08-22 12:26:41 -0700

    docs: keccak docs

--- primitives/src/keccak.rs
+++ primitives/src/keccak.rs
@@ -15,15 +15,24 @@
 use alloy_primitives::{b256, B256};
 use sha3::{Digest, Keccak256};
 
-/// Keccak hash of an empty slice.
+/// Represents the Keccak-256 hash of an empty byte slice.
+///
+/// This is a constant value and can be used as a default or placeholder
+/// in various cryptographic operations.
 pub const KECCAK_EMPTY: B256 =
     b256!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
 
-/// Computes the Keccak-256 hash.
-/// TODO: consider switching to B256 to have consistent return types
+/// Computes the Keccak-256 hash of the provided data.
+///
+/// This function is a thin wrapper around the Keccak256 hashing algorithm
+/// and is optimized for performance.
+///
+/// # TODO
+/// - Consider switching the return type to `B256` for consistency with other parts of the
+///   codebase.
 #[inline]
 pub fn keccak(data: impl AsRef<[u8]>) -> [u8; 32] {
-    // TODO: remove accelerated hashing benchmark
+    // TODO: Remove this benchmarking code once performance testing is complete.
     // std::hint::black_box(sha2::Sha256::digest(&data));
     Keccak256::digest(data).into()
 }

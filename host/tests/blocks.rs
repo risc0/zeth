a2be5bc1033479dd3071c71f559d184b330e73d6
Wolfgang Welz <welzwo@gmail.com>
2023-10-10 17:28:26 +0200

    add optimism tests

--- host/tests/blocks.rs
+++ host/tests/blocks.rs
@@ -12,17 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
 use assert_cmd::Command;
-use risc0_zkvm::{serde::to_vec, Executor, ExecutorEnv, FileSegmentRef};
 use rstest::rstest;
-use tempfile::tempdir;
-use zeth_guests::ETH_BLOCK_ELF;
-use zeth_lib::{
-    block_builder::EthereumStrategyBundle, consts::ETH_MAINNET_CHAIN_SPEC, input::Input,
-};
-use zeth_primitives::{transactions::ethereum::EthereumTxEssence, trie::MptNodeData};
 
 #[rstest]
 fn block_cli_ethereum(#[files("testdata/ethereum/*.json.gz")] path: PathBuf) {
@@ -30,50 +23,28 @@ fn block_cli_ethereum(#[files("testdata/ethereum/*.json.gz")] path: PathBuf) {
 
     Command::cargo_bin("zeth")
         .unwrap()
-        .args(["--cache=testdata", &format!("--block-no={}", block_no)])
+        .args([
+            "--network=ethereum",
+            "--cache=testdata",
+            &format!("--block-no={}", block_no),
+        ])
         .assert()
         .success();
 }
 
 #[rstest]
-fn empty_blocks(#[files("testdata/ethereum/*.json.gz")] path: PathBuf) {
-    let block_no = u64::from_str(file_prefix(&path)).unwrap();
-    // Set block cache directory
-    let rpc_cache = Some(format!("testdata/ethereum/{}.json.gz", block_no));
-    // Fetch all of the initial data
-    let init = zeth_lib::host::get_initial_data::<EthereumStrategyBundle>(
-        ETH_MAINNET_CHAIN_SPEC.clone(),
-        rpc_cache,
-        None,
-        block_no,
-    )
-    .expect("Could not init");
-    // Create input object
-    let mut input: Input<EthereumTxEssence> = init.clone().into();
-    // Take out transaction and withdrawal execution data
-    input.transactions = Default::default();
-    input.withdrawals = Default::default();
-    input.contracts = Default::default();
-    input.parent_state_trie = MptNodeData::Digest(input.parent_state_trie.hash()).into();
-    input.parent_storage = Default::default();
-    input.ancestor_headers = Default::default();
-    // Prepare executor
-    let env = ExecutorEnv::builder()
-        .session_limit(None)
-        .segment_limit_po2(20)
-        .add_input(&to_vec(&input).unwrap())
-        .build()
-        .unwrap();
-    let mut exec = Executor::from_elf(env, ETH_BLOCK_ELF).unwrap();
-    // Run Executor
-    let segment_dir = tempdir().unwrap();
-    let session = exec
-        .run_with_callback(|segment| {
-            Ok(Box::new(FileSegmentRef::new(&segment, segment_dir.path())?))
-        })
-        .unwrap();
-    // Output segment count
-    println!("Generated {} segments", session.segments.len());
+fn block_cli_optimism(#[files("testdata/optimism/*.json.gz")] path: PathBuf) {
+    let block_no = file_prefix(&path);
+
+    Command::cargo_bin("zeth")
+        .unwrap()
+        .args([
+            "--network=optimism",
+            "--cache=testdata",
+            &format!("--block-no={}", block_no),
+        ])
+        .assert()
+        .success();
 }
 
 fn file_prefix(path: &PathBuf) -> &str {

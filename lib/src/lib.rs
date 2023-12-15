2f6a865135d4be618e3cb8b5b2dee908dfa3a379
Timothy Carstens <intoverflow@gmail.com>
2023-12-15 12:56:19 -0800

    Re-enable core::mem::forget() optimization

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -27,6 +27,6 @@ pub use zeth_primitives::transactions::{ethereum::EthereumTxEssence, optimism::O
 
 /// call forget only if running inside the guest
 pub fn guest_mem_forget<T>(_t: T) {
-    // #[cfg(target_os = "zkvm")]
-    // core::mem::forget(_t)
+    #[cfg(target_os = "zkvm")]
+    core::mem::forget(_t)
 }

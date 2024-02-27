083829ec0eaf2d0d1d172b62426af9d7957b8a87
ceciliaz030 <zhangyixin319@gmail.com>
2024-02-27 23:41:59 +0800

    compiled host

--- raiko-host/src/prover/proof/risc0.rs
+++ raiko-host/src/prover/proof/risc0.rs
@@ -2,17 +2,16 @@ use std::path::PathBuf;
 
 use alloy_primitives::FixedBytes;
 use serde::{Deserialize, Serialize};
-use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 use zeth_lib::{consts::TKO_MAINNET_CHAIN_SPEC, taiko::host::HostArgs};
 
 use crate::prover::{
     consts::*,
     context::Context,
-    request::{SP1Response, SgxRequest, SgxResponse},
+    request::{SgxRequest, SgxResponse},
     utils::guest_executable_path,
 };
 
-pub async fn execute_risc0(ctx: &mut Context, req: &SgxRequest) -> Result<SP1Response, String> {
+pub async fn execute_risc0(ctx: &mut Context, req: &SgxRequest) -> Result<SgxResponse, String> {
     
     todo!()
 }

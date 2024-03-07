5a4625ba7e932da54c27859befe1deba89f0fa97
ceciliaz030 <zhangyixin319@gmail.com>
2024-03-07 12:02:56 +0800

    wip

--- pullblock.sh
+++ pullblock.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+for block in {10..10000..1}
+do
+  curl --location --request POST 'http://localhost:8080' \
+       --header 'Content-Type: application/json' \
+       --data-raw "{
+         \"jsonrpc\": \"2.0\",
+         \"id\": 1,
+         \"method\": \"proof\",
+         \"params\": [
+           {
+             \"type\": \"succinct\",
+             \"l2Rpc\": \"https://rpc.katla.taiko.xyz\",
+             \"l1Rpc\": \"https://ethereum-holesky-rpc.publicnode.com\",
+             \"l2Contracts\": \"testnet\",
+             \"proofInstance\": \"succinct\",
+             \"blockNumber\": $block,
+             \"prover\": \"0x70997970C51812dc3A010C7d01b50e0d17dc79C8\",
+             \"graffiti\": \"0000000000000000000000000000000000000000000000000000000000000000\"
+           }
+         ]
+       }"
+done
+
+
+# https://ethereum-holesky-rpc.publicnode.com 🉑️
+
+# #!/bin/bash
+
 # for block in {10..10000..1}
 # do
 #   curl --location --request POST 'http://localhost:8080' \
@@ -14,7 +42,14 @@
 #              \"l2Rpc\": \"https://rpc.katla.taiko.xyz\",
 #              \"l1Rpc\": \"https://ethereum-holesky-rpc.publicnode.com\",
 #              \"l2Contracts\": \"testnet\",
-#              \"proofInstance\": \"risc0\",
+#              \"proofInstance\": {
+#                \"risc0\": {
+#                  \"bonsai\": false,
+#                  \"snark\": false,
+#                  \"profile\": true,
+#                  \"execution_po2\": 18
+#                }
+#              },
 #              \"blockNumber\": $block,
 #              \"prover\": \"0x70997970C51812dc3A010C7d01b50e0d17dc79C8\",
 #              \"graffiti\": \"0000000000000000000000000000000000000000000000000000000000000000\"
@@ -22,38 +57,3 @@
 #          ]
 #        }"
 # done
-
-
-# https://ethereum-holesky-rpc.publicnode.com 🉑️
-
-#!/bin/bash
-
-for block in {10..10000..1}
-do
-  curl --location --request POST 'http://localhost:8080' \
-       --header 'Content-Type: application/json' \
-       --data-raw "{
-         \"jsonrpc\": \"2.0\",
-         \"id\": 1,
-         \"method\": \"proof\",
-         \"params\": [
-           {
-             \"type\": \"risc0\",
-             \"l2Rpc\": \"https://rpc.katla.taiko.xyz\",
-             \"l1Rpc\": \"https://ethereum-holesky-rpc.publicnode.com\",
-             \"l2Contracts\": \"testnet\",
-             \"proofInstance\": {
-               \"risc0\": {
-                 \"bonsai\": false,
-                 \"snark\": false,
-                 \"profile\": true,
-                 \"execution_po2\": 18
-               }
-             },
-             \"blockNumber\": $block,
-             \"prover\": \"0x70997970C51812dc3A010C7d01b50e0d17dc79C8\",
-             \"graffiti\": \"0000000000000000000000000000000000000000000000000000000000000000\"
-           }
-         ]
-       }"
-done
--- raiko-host/src/main.rs
+++ raiko-host/src/main.rs
@@ -14,7 +14,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+#![feature(stdsimd)]
+#![feature(avx512_target_feature)]
 mod metrics;
 mod prover;
 #[allow(dead_code)]

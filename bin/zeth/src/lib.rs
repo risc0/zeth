bd2e9345d236fdc70d41113666f2f3e0bcd70118
Rami Khalil <the.rami.khalil@gmail.com>
2024-10-23 12:04:10 +0300

    downgrade reth msrv + cleanup

--- bin/zeth/src/lib.rs
+++ bin/zeth/src/lib.rs
@@ -13,55 +13,13 @@
 // limitations under the License.
 
 use alloy::primitives::B256;
-use log::debug;
 use risc0_zkvm::{is_dev_mode, ProverOpts};
-use std::fs;
-use std::path::Path;
 use zeth_core::keccak::keccak;
 
 pub mod cli;
 pub mod client;
 pub mod executor;
 pub mod result;
-
-pub fn load_receipt<T: serde::de::DeserializeOwned>(
-    file_name: &String,
-) -> anyhow::Result<Option<(String, T)>> {
-    if is_dev_mode() {
-        // Nothing to load
-        return Ok(None);
-    }
-
-    let receipt_serialized = match fs::read(zkp_cache_path(file_name)) {
-        Ok(receipt_serialized) => receipt_serialized,
-        Err(err) => {
-            debug!("Could not load cached receipt with label: {}", &file_name);
-            debug!("{:?}", err);
-            return Ok(None);
-        }
-    };
-
-    Ok(Some(bincode::deserialize(&receipt_serialized)?))
-}
-
-pub fn save_receipt<T: serde::Serialize>(receipt_label: &String, receipt_data: &(String, T)) {
-    if !is_dev_mode() {
-        fs::write(
-            zkp_cache_path(receipt_label),
-            bincode::serialize(receipt_data).expect("Failed to serialize receipt!"),
-        )
-        .expect("Failed to save receipt output file.");
-    }
-}
-
-fn zkp_cache_path(receipt_label: &String) -> String {
-    let dir = Path::new("cache_zkp");
-    fs::create_dir_all(dir).expect("Could not create directory");
-    dir.join(format!("{}.zkp", receipt_label))
-        .to_str()
-        .unwrap()
-        .to_string()
-}
 
 pub fn proof_file_name(
     first_block_hash: B256,

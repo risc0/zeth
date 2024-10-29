4a379a021dd078b121e31a16c3c676359422f61e
Rami Khalil <the.rami.khalil@gmail.com>
2024-10-29 14:42:09 +0300

    fmt

--- guests/build.rs
+++ guests/build.rs
@@ -16,17 +16,18 @@
 fn main() {
     let cwd = std::env::current_dir().unwrap();
     let root_dir = cwd.parent().map(|d| d.to_path_buf());
-    let build_opts = std::collections::HashMap::from_iter(["zeth-guests-reth"].into_iter().map(|guest_pkg| {
-        (
-            guest_pkg,
-            risc0_build::GuestOptions {
-                features: vec![],
-                use_docker: Some(risc0_build::DockerOptions {
-                    root_dir: root_dir.clone(),
-                }),
-            },
-        )
-    }));
+    let build_opts =
+        std::collections::HashMap::from_iter(["zeth-guests-reth"].into_iter().map(|guest_pkg| {
+            (
+                guest_pkg,
+                risc0_build::GuestOptions {
+                    features: vec![],
+                    use_docker: Some(risc0_build::DockerOptions {
+                        root_dir: root_dir.clone(),
+                    }),
+                },
+            )
+        }));
     risc0_build::embed_methods_with_options(build_opts);
 }
 

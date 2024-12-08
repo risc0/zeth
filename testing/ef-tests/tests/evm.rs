52219624499bb596438079e4ccfb470476248950
Wolfgang Welz <welzwo@gmail.com>
2024-12-08 11:53:52 +0100

    Re-add eth execution tests

--- testing/ef-tests/tests/evm.rs
+++ testing/ef-tests/tests/evm.rs
@@ -14,24 +14,19 @@
 
 #![cfg(feature = "ef-tests")]
 
-use std::path::PathBuf;
-
+use alloy::network::Ethereum;
 use rstest::rstest;
-use zeth_lib::{
-    builder::{BlockBuilderStrategy, EthereumStrategy},
-    output::BlockBuildOutput,
-};
-use zeth_primitives::{block::Header, trie::StateAccount};
-use zeth_testeth::{
-    create_input, ethers,
-    ethtests::{read_eth_test, EthTestCase},
-};
+use std::{cell::RefCell, path::PathBuf, rc::Rc};
+use zeth_preflight::{client::PreflightClient, BlockBuilder, Witness};
+use zeth_preflight_ethereum::{RethBlockBuilder, RethPreflightDriver};
+use zeth_testeth::{read_eth_execution_tests, TestCoreDriver, TestProvider};
 
 #[rstest]
 fn evm(
     #[files("testdata/BlockchainTests/GeneralStateTests/**/*.json")]
     #[exclude("RevertPrecompiledTouch_storage.json|RevertPrecompiledTouch.json")] // precompiles having storage is not possible
-    #[exclude("stTimeConsuming")] // exclude only the time consuming tests
+    #[exclude("RevertInCreateInInit_Paris.json|RevertInCreateInInit.json|dynamicAccountOverwriteEmpty.json|dynamicAccountOverwriteEmpty_Paris.json|RevertInCreateInInitCreate2Paris.json|create2collisionStorage.json|RevertInCreateInInitCreate2.json|create2collisionStorageParis.json|InitCollision.json|InitCollisionParis.json")] // Test with some storage check
+    #[exclude("stTimeConsuming")] // exclude only the time-consuming tests
     path: PathBuf,
 ) {
     let _ = env_logger::builder()
@@ -39,14 +34,12 @@ fn evm(
         .is_test(true)
         .try_init();
 
-    for EthTestCase {
-        mut json,
-        genesis,
-        chain_spec,
-    } in read_eth_test(path)
-    {
+    for mut json in read_eth_execution_tests(path) {
         // only one block supported for now
-        assert_eq!(json.blocks.len(), 1);
+        if json.blocks.len() > 1 {
+            println!("skipping (blockchain)");
+            break;
+        };
         let block = json.blocks.pop().unwrap();
 
         // skip failing tests for now
@@ -55,57 +48,26 @@ fn evm(
             break;
         }
 
-        let block_header = block.block_header.unwrap();
-        let expected_header: Header = block_header.clone().into();
-        assert_eq!(&expected_header.hash(), &block_header.hash);
+        let expected_header = block.block_header.as_ref().unwrap();
+        let expected_hash = expected_header.hash;
 
         // using the empty/default state for the input prepares all accounts for deletion
         // this leads to larger input, but can never fail
-        let post_state = json.post.clone().unwrap_or_default();
+        let post_state = json.post_state.clone().unwrap_or_default();
 
-        let input = create_input(
-            &chain_spec,
-            genesis,
-            json.pre,
-            expected_header.clone(),
-            block.transactions,
-            block.withdrawals.unwrap_or_default(),
-            post_state,
-        );
-        let input_state_input_hash = input.state_input.hash();
+        let provider = TestProvider::new(json.genesis_block_header, block, json.pre, post_state);
+        let provider = Rc::new(RefCell::new(provider));
 
-        let output = EthereumStrategy::build_from(&chain_spec, input).unwrap();
+        let preflight_data =
+            <<RethBlockBuilder as BlockBuilder<_, _, TestCoreDriver, _>>::PreflightClient as PreflightClient<
+                Ethereum,
+                TestCoreDriver,
+                RethPreflightDriver,
+            >>::preflight_with_provider(provider, 1, 1)
+                .unwrap();
+        let build_result = Witness::driver_from::<TestCoreDriver>(&preflight_data);
 
-        let BlockBuildOutput::SUCCESS {
-            hash: new_block_hash,
-            head: new_block_head,
-            state: new_block_state,
-            state_input_hash,
-        } = output
-        else {
-            panic!("Invalid block")
-        };
-
-        if let Some(post) = json.post {
-            let (exp_state, _) = ethers::build_tries(&post);
-
-            println!("diffing state trie:");
-            for diff in diff::slice(
-                &new_block_state.debug_rlp::<StateAccount>(),
-                &exp_state.debug_rlp::<StateAccount>(),
-            ) {
-                match diff {
-                    diff::Result::Left(l) => println!("✗{}", l),
-                    diff::Result::Right(r) => println!("✓{}", r),
-                    diff::Result::Both(l, _) => println!(" {}", l),
-                }
-            }
-            assert_eq!(new_block_state.hash(), exp_state.hash());
-        }
-
-        // the headers should match
-        assert_eq!(new_block_head, expected_header);
-        assert_eq!(new_block_hash, expected_header.hash());
-        assert_eq!(input_state_input_hash, state_input_hash);
+        // the header should match
+        assert_eq!(build_result.validated_tip_hash, expected_hash);
     }
 }

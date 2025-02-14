8c3d205b7aabc738a3ccc406f3947cff807de392
Wolfgang Welz <welzwo@gmail.com>
2025-02-14 20:07:32 +0100

    Use a builder pattern for GuestOptions and DockerOptions

--- guests/build.rs
+++ guests/build.rs
@@ -15,21 +15,21 @@
 #[cfg(not(any(feature = "debug-guest-build", debug_assertions)))]
 fn main() {
     let cwd = std::env::current_dir().unwrap();
-    let root_dir = cwd.parent().map(|d| d.to_path_buf());
+    let root_dir = cwd.parent().expect("No parent directory");
+
+    let docker_options = risc0_build::DockerOptionsBuilder::default()
+        .root_dir(root_dir)
+        .build()
+        .unwrap();
+    let guest_options = risc0_build::GuestOptionsBuilder::default()
+        .use_docker(docker_options)
+        .build()
+        .unwrap();
+
     let build_opts = std::collections::HashMap::from_iter(
         ["zeth-guests-reth-ethereum", "zeth-guests-reth-optimism"]
             .into_iter()
-            .map(|guest_pkg| {
-                (
-                    guest_pkg,
-                    risc0_build::GuestOptions {
-                        features: vec![],
-                        use_docker: Some(risc0_build::DockerOptions {
-                            root_dir: root_dir.clone(),
-                        }),
-                    },
-                )
-            }),
+            .map(|guest_pkg| (guest_pkg, guest_options.clone())),
     );
     risc0_build::embed_methods_with_options(build_opts);
 }

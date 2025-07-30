faecacef7a1adbaf5d3b037e98ea85c16befb531
Wolfgang Welz <welzwo@gmail.com>
2025-07-30 15:05:59 +0200

    fix clippy warning

--- guests/build.rs
+++ guests/build.rs
@@ -32,7 +32,7 @@ fn main() {
 
     // Use Docker for deterministic builds if RISC0_USE_DOCKER is set.
     if env::var("RISC0_USE_DOCKER").is_ok() {
-        let docker_tag = format!("r0.{}", RISC0_RUST_VERSION);
+        let docker_tag = format!("r0.{RISC0_RUST_VERSION}");
         println!("cargo:warning=Using Docker build with tag: {}", &docker_tag);
 
         let docker_opts = DockerOptionsBuilder::default()
